dbb0c7958ea93b2f0c610f73e1ba6d629eef094c
bilel <bilelh532@gmail.com>
2019-05-31 15:37:08 +0200

    TP2 5. Editer POST DONE   ==>  TP DONE

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,6 +1,8 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +13,6 @@ public class EditerCollaborateurController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 	ServletException, IOException {
-		
 		// recupere la valeur d'un parametre dont le nom est matricule
 		String matriculeParam = req.getParameter("matricule");
 		resp.setContentType("text/html");
@@ -31,4 +32,51 @@ public class EditerCollaborateurController extends HttpServlet {
 			+ "</ul>");
 		}
 	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
+	ServletException, IOException {
+		String matriculeParam = req.getParameter("matricule");
+		String titreParam = req.getParameter("titre");
+		String nomParam = req.getParameter("nom");
+		String prenomParam = req.getParameter("prenom");
+		
+		if(req.getParameter("matricule")==null 
+				|| req.getParameter("titre")==null 
+				|| req.getParameter("nom")==null 
+				|| req.getParameter("prenom")==null) {
+			List incorrect = new ArrayList();
+			
+				if(req.getParameter("matricule")==null) {
+					incorrect.add("matricule");
+				}
+				if (req.getParameter("titre")==null) {
+					incorrect.add("titre");
+				}
+				if (req.getParameter("nom")==null) {
+					incorrect.add("nom");
+				}
+				if (req.getParameter("prenom")==null) {
+					incorrect.add("prenom");
+				}
+			
+			resp.setStatus(400);
+			resp.getWriter().write("<h1>Edition de collaborateur</h1>" 
+					+ "<ul>"
+					+ "<li>Les parametres suivants sont incorrects: " + incorrect + "</li>" 
+					+ "</ul>");
+			
+		}else {
+			// code HTML ecrit dans le corps de la reponse
+			resp.setStatus(201);
+			resp.getWriter().write("<h1>Edition de collaborateur</h1>" 
+			+ "<ul>" 
+			+ "<li>Matricule =" + matriculeParam+ ", titre = " + titreParam 
+			+ ", nom = " + nomParam + ", prenom = " + prenomParam + "</li>" 
+			+ "</ul>");
+		}
+		
+	}
+	
+	
+	
 }
