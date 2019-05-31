492709aa6a8d2df33e002fd95796abaa6b58e6d7
bilel <bilelh532@gmail.com>
2019-05-31 15:49:20 +0200

    TP3 modif de la classe ListerCollaborateursController

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -11,18 +11,7 @@ public class ListerCollaborateursController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 	ServletException, IOException {
-		// recupere la valeur d'un parametre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
-
-		// recupere la valeur d'un parametre dont le nom est departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-
-		// code HTML ecrit dans le corps de la reponse
-		resp.getWriter().write("<h1>Liste de collaborateur</h1>"
-		+ "<ul>"
-		+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-		+ "<li>departement="+ departementParam + "</li>"
-		+ "</ul>");
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
+		.forward(req, resp);
 	}
 }
